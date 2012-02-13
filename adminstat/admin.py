c8d086ed9df2fe644d684495e937f73a8638ea76
Ivan Gromov <ivan.gromov@redsolution.ru>
2012-02-13 18:53:20 +0600

    Fixed admin list view fields

--- adminstat/admin.py
+++ adminstat/admin.py
@@ -6,12 +6,12 @@ from django.contrib.admin.models import LogEntry
 class HistoryAdmin(admin.ModelAdmin):
     model = LogEntry
 
-    list_display = ('user', 'action_time', 'change_message', 'content_object')
+    list_display = ('user', 'action_time', 'change_message', 'content_type', 'content_object')
     list_filter = ('user', )
     date_hierarchy = 'action_time'
 
     def content_object(self, obj):
-        return obj.content_object
+        return obj.object_repr
     content_object.admin_order_field = 'object_id'
 
 admin.site.register(LogEntry, HistoryAdmin)
